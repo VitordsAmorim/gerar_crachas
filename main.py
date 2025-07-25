2cdbfd300e7cb701818bfda9fcd95481cefeabb7
VitordsAmorim <amorim.vitor.ds@gmail.com>
2025-07-24 22:28:43 -0300

    dados gerados aleatoriamente

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from preparar_impressao import preparar_impressao_em_memoria
 def main():
     base_dir = os.path.dirname(__file__)
     inputs_dir = os.path.join(base_dir, "inputs")
-    modelo_padrao_path = os.path.join(inputs_dir, "cracha_servos.png")
+    modelo_padrao_path = os.path.join(inputs_dir, "padrao.png")
     csv_path = os.path.join(base_dir, "data", "dados.csv")
     fonte_path = os.path.join(base_dir, "fonts", "Almendra", "Almendra-Regular.ttf")
     pdf_output_dir = os.path.join(base_dir, "outputs", "pdfs")
